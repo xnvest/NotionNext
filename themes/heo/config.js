26d57de736675581e8008a28e66958a9fbe2016b
Xponential Investing <163086824+xnvest@users.noreply.github.com>
2024-03-12 19:13:53 +0700

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,31 +1,27 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2024-01-01',
 
-  // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: 'Welcome to Xponential Investing', url: 'https://clicky.id/xnvesting' },
+    { title: 'Visit Blog for More', url: 'https://clicky.id/xnvesting' }
   ],
 
-  // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: false,
-  // 博客主体区左右侧组件颠倒位置
   HEO_HERO_BODY_REVERSE: false,
 
-  // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: 'Searching',
+  HEO_HERO_TITLE_2: 'Collecting',
+  HEO_HERO_TITLE_3: 'XNVEST.COM',
+  HEO_HERO_TITLE_4: 'Sharing',
+  HEO_HERO_TITLE_5: 'Creating',
+  HEO_HERO_TITLE_LINK: 'https://clicky.id/xnvesting',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: 'Tools', url: '/tag/Tools' },
+  HEO_HERO_CATEGORY_2: { title: 'Sites', url: '/tag/Sites' },
+  HEO_HERO_CATEGORY_3: { title: 'Tutorials', url: '/tag/Text' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -34,7 +30,7 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
+    'Hi！This is',
     '🔍 分享与热心帮助',
     '🤝 专修交互与设计',
     '🏃 脚踏实地行动派',
@@ -105,9 +101,9 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
+  HEO_SOCIAL_CARD_TITLE_1: 'YouTube Channel',
+  HEO_SOCIAL_CARD_TITLE_2: 'Join Our Channel for More',
+  HEO_SOCIAL_CARD_TITLE_3: 'Click to Join',
   HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
 
   // *****  以下配置无效，只是预留开发 ****
